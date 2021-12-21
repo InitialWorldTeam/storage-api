b7cc9c1c0a95a4afc5efe0c60478350261c30b23
d5c5ceb0 <d5c5ceb0@gmail.com>
2021-12-21 11:51:42 +0800

    upload folder

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var axios = require('axios');
 var FormData = require('form-data');
 var fs = require('fs');
+var path = require('path');
 
-exports.upfile = function(host, fpath, fname, res) {
+exports.upfile = function(host, fname, res) {
 	var data = new FormData();
 	data.append('filese', fs.createReadStream(fname));
 
@@ -22,59 +23,64 @@ exports.upfile = function(host, fpath, fname, res) {
 		});
 }
 
+
 function getAllFiles(dirPath, originalPath, originalPath2, arrayOfFiles) {
-  files = fs.readdirSync(dirPath)
+	files = fs.readdirSync(dirPath)
 
-  arrayOfFiles = arrayOfFiles || []
-  originalPath = originalPath || path.resolve(dirPath, "..")
-  originalPath2 = originalPath2 || path.resolve(dirPath, ".")
+	arrayOfFiles = arrayOfFiles || []
+	originalPath = originalPath || path.resolve(dirPath, "..")
+	originalPath2 = originalPath2 || path.resolve(dirPath, ".")
 
-  folder = path.relative(originalPath, path.join(dirPath, "/"))
+	folder = path.relative(originalPath, path.join(dirPath, "/"))
 
-  //arrayOfFiles.push({
-  //    path: folder.replace(/\\/g, "/"),
-  //})
+	//arrayOfFiles.push({
+	//    path: folder.replace(/\\/g, "/"),
+	//})
 
-  files.forEach(function (file) {
-      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-          arrayOfFiles = getAllFiles(dirPath + "/" + file, originalPath, originalPath2, arrayOfFiles)
-      } else {
-          fpath = path.join(dirPath, "/", file)
+	files.forEach(function (file) {
+		if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+			arrayOfFiles = getAllFiles(dirPath + "/" + file, originalPath, originalPath2, arrayOfFiles)
+		} else {
+			fpath = path.join(dirPath, "/", file)
 
-          arrayOfFiles.push({
-              folder: path.relative(originalPath, fpath).replace(/\\/g, "/"),
-              folder2: folder,
-              folder3: path.relative(originalPath2, fpath).replace(/\\/g, "/"),
-              //content: fs.readFileSync(fpath),
-              path: fpath,
-              filename: file
-          })
-      }
-  })
+			arrayOfFiles.push({
+				folder: path.relative(originalPath, fpath).replace(/\\/g, "/"),
+				folder2: folder,
+				folder3: path.relative(originalPath2, fpath).replace(/\\/g, "/"),
+				//content: fs.readFileSync(fpath),
+				path: fpath,
+				filename: file
+			})
+		}
+	})
 
-  return arrayOfFiles
+	return arrayOfFiles
 }
 
+exports.upfolder = function(host, fpath, res) {
 
-exports.upfolder = async function(host, fpath, res) {
-    let files = getAllFiles(fpath)
-    for (file of files) {
-        var data = new FormData();
-        data.append(file.folder2, fs.createReadStream(file.path));
-    }
+	originalPath = path.resolve(fpath, "..")
+	ofolder = path.relative(originalPath, path.join(fpath, "/"))
 
-    var config = {
-        method: 'post',
-        url: `${host}/api/v1/files`,
-        headers: {
-            ...data.getHeaders()
-        },
-        data : data
-    };
+	let files = getAllFiles(fpath)
+	var data = new FormData();
+	for (file of files) {
+		data.append(file.folder2, fs.createReadStream(file.path));
+	}
 
-        let res = await axios(config);
-        console.log(res)
-    }
+	var config = {
+		method: 'post',
+		url: `${host}/api/v1/folder`,
+		headers: { 
+			folder: ofolder,
+			...data.getHeaders()
+		},
+		data : data
+	};
 
-    //TODO pinfiles
+	axios(config)
+		.then(res)
+		.catch(function (error) {
+			throw new Error(error)
+		});
 }
